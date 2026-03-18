3295c7f4e1b60404b48a3f5c76730c908daba721
ShutongZhang2023 <sz3832@nyu.edu>
2026-03-17 22:34:47 -0400

    create new project page finished

--- front-end/src/CreateProjectForm.js
+++ front-end/src/CreateProjectForm.js
@@ -22,6 +22,82 @@ const generOption = [
   { value: "Straregy", label: "Straregy" },
 ];
 
+function UploadSection({
+  uploadType,
+  setUploadType,
+  uploadFile,
+  setUploadFile,
+  uploadUrl,
+  setUploadUrl,
+}) {
+  const handleUploadFileChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setUploadFile(file);
+  };
+
+  let content;
+
+  if (uploadType === "download") {
+    content = (
+      <div className="upload-mode-content">
+        <label htmlFor="project-upload-file" className="upload-file-button">
+          Choose File
+        </label>
+
+        <input
+          id="project-upload-file"
+          type="file"
+          onChange={handleUploadFileChange}
+          style={{ display: "none" }}
+        />
+
+        {uploadFile && (
+          <p className="upload-file-name">Selected: {uploadFile.name}</p>
+        )}
+      </div>
+    );
+  } else {
+    content = (
+      <div className="upload-mode-content">
+        <input
+          type="text"
+          placeholder="Enter project URL"
+          value={uploadUrl}
+          onChange={(e) => setUploadUrl(e.target.value)}
+          className="single-line-input"
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="upload-tabs">
+        <button
+          type="button"
+          className={
+            uploadType === "download" ? "upload-tab active" : "upload-tab"
+          }
+          onClick={() => setUploadType("download")}
+        >
+          Downloadable
+        </button>
+
+        <button
+          type="button"
+          className={uploadType === "url" ? "upload-tab active" : "upload-tab"}
+          onClick={() => setUploadType("url")}
+        >
+          URL
+        </button>
+      </div>
+
+      <div>{content}</div>
+    </div>
+  );
+}
+
 function CreateProjectForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -31,8 +107,14 @@ function CreateProjectForm() {
   const [coverImage, setCoverImage] = useState(null);
   const [coverPreview, setCoverPreview] = useState("");
 
-  const handleCoverUpload = (out) => {
-    const file = out.target.files[0];
+  const [uploadType, setUploadType] = useState("download");
+  const [uploadFile, setUploadFile] = useState(null);
+  const [uploadUrl, setUploadUrl] = useState("");
+
+  const [visibility, setVisibility] = useState("");
+
+  const handleCoverUpload = (e) => {
+    const file = e.target.files[0];
     if (!file) return;
 
     setCoverImage(file);
@@ -41,21 +123,59 @@ function CreateProjectForm() {
     setCoverPreview(imageUrl);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      title,
+      description,
+      genre,
+      tags,
+      visibility,
+      uploadType,
+      coverImage,
+      uploadFile,
+      uploadUrl,
+    };
+    console.log("Submitted Data:", formData);
+    alert("Form submitted then nacigate to project info page");
+    //navigate("/project", { state: formData });
+  };
+
+  const handleDiscard = () => {
+    const confirmClear = window.confirm("Discard all changes?");
+    if (!confirmClear) return;
+
+    setTitle("");
+    setDescription("");
+    setGenre("");
+    setTags("");
+    setVisibility("");
+    setUploadType("download");
+    setCoverImage(null);
+    setCoverPreview("");
+    setUploadFile(null);
+    setUploadUrl("");
+  };
+
   return (
     <div className="container">
+      <div className="top-nav">
+        <span className="nav-link"> Dashboard</span>
+      </div>
       <div className="header">Create a New Project!</div>
 
-      <form>
-        <div className="infoContainer">
+      <form onSubmit={handleSubmit}>
+        <div className="info-container">
           <label>Title</label>
           <input
             type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
+            className="single-line-input"
           />
         </div>
 
-        <div className="infoContainer">
+        <div className="info-container">
           <label>Short description</label>
           <textarea
             value={description}
@@ -63,7 +183,7 @@ function CreateProjectForm() {
           />
         </div>
 
-        <div className="infoContainer">
+        <div className="info-container">
           <label> Genre </label>
           <TagSelector
             value={genre}
@@ -73,7 +193,7 @@ function CreateProjectForm() {
           />
         </div>
 
-        <div className="infoContainer">
+        <div className="info-container">
           <label> Tag </label>
           <TagSelector
             value={tags}
@@ -83,7 +203,7 @@ function CreateProjectForm() {
           />
         </div>
 
-        <div className="infoContainer">
+        <div className="info-container">
           <label>Cover Image</label>
 
           <input
@@ -110,7 +230,64 @@ function CreateProjectForm() {
               </div>
             )}
           </label>
+        </div>
 
+        <div className="form-section">
+          <h3>Uploads</h3>
+          <UploadSection
+            uploadType={uploadType}
+            setUploadType={setUploadType}
+            uploadFile={uploadFile}
+            setUploadFile={setUploadFile}
+            uploadUrl={uploadUrl}
+            setUploadUrl={setUploadUrl}
+          />
+        </div>
+
+        <div className="form-section">
+          <h3>Visibility & access</h3>
+          <label className="radio-label">
+            <input
+              type="radio"
+              name="visibility"
+              value="draft"
+              checked={visibility === "draft"}
+              onChange={(e) => setVisibility(e.target.value)}
+            />
+            <span>
+              <strong>Draft</strong> - Only those who can edit the project can
+              view the page
+            </span>
+          </label>
+          <br/>
+          <label className="radio-label">
+            <input
+              type="radio"
+              name="visibility"
+              value="public"
+              checked={visibility === "public"}
+              onChange={(e) => setVisibility(e.target.value)}
+            />
+            <span>
+              <strong>Public</strong> - Anyone can view the page
+            </span>
+          </label>
+        </div>
+
+        <div>
+          <button type="submit" className="basic-button">
+            Save and View Pages
+          </button>
+        </div>
+
+        <div>
+          <button
+            type="button"
+            className="basic-button"
+            onClick={handleDiscard}
+          >
+            Discard
+          </button>
         </div>
       </form>
     </div>
--- front-end/src/TagSelector.js
+++ front-end/src/TagSelector.js
@@ -1,15 +1,50 @@
 import CreatableSelect from "react-select/creatable";
 
-function TagSelector({ value, onChange, options, isMulti = true }){
-    return(
-        <CreatableSelect
-            isMulti = {isMulti}
-            options = {options}
-            value={value}
-            onChange={onChange}
-            placeholder="Search or create tags..."
-        />
-    );
+function TagSelector({ value, onChange, options, isMulti = true }) {
+  const customStyles = {
+    control: (base, state) => ({
+      ...base,
+      borderColor: state.isFocused ? "#14112E" : "rgb(204, 204, 204)",
+      boxShadow: state.isFocused ? "0 0 0 2px rgba(130, 123, 255, 0.329)" : "none",
+    }),
+    option: (base, state) => ({
+      ...base,
+      backgroundColor: state.isSelected
+        ? "#14112E"
+        : state.isFocused
+          ? "#d2d0f7"
+          : "white",
+      color: state.isSelected ? "white" : "#333",
+      ":active": {
+        backgroundColor: "#14112E",
+      },
+    }),
+    multiValueLabel: (base) => ({
+      ...base,
+      color: "#ffffff",
+      backgroundColor: "#14112ebe",
+    }),
+
+    multiValueRemove: (base) => ({
+      ...base,
+      color: "#ffffff",
+      backgroundColor: "#14112ebe",
+      ":hover": {
+        backgroundColor: "#3d348b8f"
+      },
+    }),
+  };
+
+  return (
+    <CreatableSelect
+      isMulti={isMulti}
+      options={options}
+      value={value}
+      onChange={onChange}
+      placeholder="Search or create tags..."
+      styles={customStyles}
+    />
+  );
 }
 
 export default TagSelector;
