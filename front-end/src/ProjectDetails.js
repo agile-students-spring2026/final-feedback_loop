029445044c33ca30babdda0f7682670ff05e6657
Xuan4781 <Xuan4781@users.noreply.github.com>
2026-03-21 16:00:41 -0400

    feat: base layout for projectdetails

---
+++ front-end/src/ProjectDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ProjectDetails.css";
+
+const ProjectDetails = ({ game, setActiveTab, handleJoinPlaytest }) => {
+  if (!game) return null;
+
+  return <div className="project-wrapper"></div>;
+};
+
+export default ProjectDetails;
