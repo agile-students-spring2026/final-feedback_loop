962286ee4c1b97c90ed3394c2f99d5f551655385
Xuan4781 <Xuan4781@users.noreply.github.com>
2026-03-21 15:23:21 -0400

    add join playtest

--- front-end/src/PlayerPlaytest.js
+++ front-end/src/PlayerPlaytest.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const PlayerPlaytest = ({ games }) => {
+const PlayerPlaytest = ({ games, handleJoinPlaytest }) => {
   return (
     <div>
       <h1>Explore Projects</h1>
@@ -9,6 +9,10 @@ const PlayerPlaytest = ({ games }) => {
           <div key={game.id}>
             <h3>{game.title}</h3>
             <p>{game.description}</p>
+
+            <button onClick={() => handleJoinPlaytest(game)}>
+              Join Test
+            </button>
           </div>
         ))}
       </div>
