518567055a0236ac2766d4a9dd5758c42d918f01
Xuan4781 <Xuan4781@users.noreply.github.com>
2026-03-21 16:02:28 -0400

    add launcher header with nav layout

--- front-end/src/ProjectDetails.js
+++ front-end/src/ProjectDetails.js
@@ -4,7 +4,23 @@ import "./ProjectDetails.css";
 const ProjectDetails = ({ game, setActiveTab, handleJoinPlaytest }) => {
   if (!game) return null;
 
-  return <div className="project-wrapper"></div>;
+  return (
+    <div className="project-wrapper">
+      {/* Header */}
+      <div className="launcher-header">
+        <button
+          className="back-btn"
+          onClick={() => setActiveTab("Explore")}
+        >
+          Back
+        </button>
+
+        <span className="launcher-title">Game Launcher</span>
+
+        <div className="header-spacer" />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectDetails;
