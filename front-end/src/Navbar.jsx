f164c313302fb5569c6eb6ad138198324c7bd73b
fh2303 <fepaul82@gmail.com>
2026-04-20 23:23:07 -0400

    Added app logo and hosted it

--- front-end/src/Navbar.jsx
+++ front-end/src/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import listIcon from "./assets/list.svg"
+import listIcon from "./assets/list.svg";
 import { getUser } from "./api";
 
 const Navbar = ({ onBurgerClick }) => {
@@ -9,7 +9,13 @@ const Navbar = ({ onBurgerClick }) => {
       <button className="burger" onClick={onBurgerClick}>
         <img src={listIcon} alt="menu" className="burger-icon" />
       </button>
-      <div className="logo">[ LOGO ]</div>
+      <div className="appLogo">
+        <img
+          src="https://res.cloudinary.com/dpdidryxs/image/upload/v1776740679/b53003b87e9aa0e57c4b63bd5d18db06_phrqp9.png"
+          alt="Logo"
+          style={{ height: "40px", width: "auto" }}
+        />
+      </div>
       {user?.username && (
         <span style={{ marginLeft: "auto", fontSize: "0.75rem", opacity: 0.8 }}>
           @{user.username}
